e1c9074c52734a76d69f8697bb8834e88e4913be
Ollie Foster <ollie_foster@btinternet.com>
2024-11-05 10:40:29 +0000

    new file:   app.py 	new file:   static/favicon.ico 	new file:   static/logo.jpg 	new file:   static/main.css 	new file:   static/main.js 	new file:   templates/aboutUs.html 	new file:   templates/contact.html 	new file:   templates/index.html 	new file:   templates/products.html

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/aboutUs")
+def about_us():
+    return render_template("aboutUs.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
